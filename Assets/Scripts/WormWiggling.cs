6bede29b81ba2d928ac5d496e022af911d4805a7
itamar <itamarcu@gmail.com>
2024-08-18 18:48:06 +0300

    Final touches for Larva scene

--- Assets/Scripts/WormWiggling.cs
+++ Assets/Scripts/WormWiggling.cs
@@ -12,30 +12,36 @@ public class WormWiggling : MonoBehaviour
     public float wiggleAngle;
     public float wiggleHeight;
     public Animator cinematicAntAnimator;
+    public GameObject wiggleInstructions;
 
     private Transform _transChild;
+    private SpriteRenderer _spriteChild;
     private Animator _anim;
 
     private float _totalWiggleTime = 0.000001f;
     private Vector2 _inputVector = new(0.0f, 0.0f);
     private static readonly int AnimMoving = Animator.StringToHash("moving");
-    private bool eventTriggered = false;
+    private bool _antApproaching = false;
+    private bool _antReached = false;
 
     public UnityEvent WormEaten;
 
     void Awake()
     {
         _transChild = gameObject.GetComponentsInChildren<Transform>()[1];
+        _spriteChild = gameObject.GetComponentInChildren<SpriteRenderer>();
         _anim = gameObject.GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        _inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        _inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
     }
 
     void FixedUpdate()
     {
+        if (_antReached)
+            return;
         var keepWiggling = _inputVector.magnitude > 0.0f || Math.Abs(_transChild.localRotation.z) > 0.02;
         if (keepWiggling)
         {
@@ -53,9 +59,14 @@ public class WormWiggling : MonoBehaviour
             _anim.SetBool(AnimMoving, false);
         }
 
-        if (_totalWiggleTime > 4 && !eventTriggered)
+        if (_totalWiggleTime > 2)
         {
-            eventTriggered = true;
+            wiggleInstructions.SetActive(false);
+        }
+
+        if (_totalWiggleTime > 4 && !_antApproaching)
+        {
+            _antApproaching = true;
             cinematicAntAnimator.SetTrigger("go_1");
             StartCoroutine(WaitForEndOfLevel());
         }
@@ -63,7 +74,12 @@ public class WormWiggling : MonoBehaviour
 
     private IEnumerator WaitForEndOfLevel()
     {
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(2);
+        _antReached = true;
+        // remove sprite renderer
+        yield return new WaitForSeconds(1);
+        _spriteChild.enabled = false;
+        yield return new WaitForSeconds(1);
         WormEaten?.Invoke();
     }
 }
