be010037f08c3095d106622007bcb1a71dc1850f
itamar <itamarcu@gmail.com>
2024-08-19 11:13:52 +0300

    Fix eagle controller more - now has sounds and feedback, good speed, dash, etc

--- Assets/Scripts/AnimalControllers/EagleController.cs
+++ Assets/Scripts/AnimalControllers/EagleController.cs
@@ -8,69 +8,93 @@ using UnityEngine.InputSystem;
 
 public class EagleController : AnimalController
 {
-    public AudioClip screechSound;
-    
-    [SerializeField] private float forwardSpeed = 20f;
-    [SerializeField] private float verticalSpeed = 10f;
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float rotationAngle;
+    [SerializeField] private float minimumDashDuration;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float delayBetweenAttacks;
     [SerializeField] private MMF_Player SquashStretch;
+    [SerializeField] private AudioClip[] screechSounds;
+    [SerializeField] private AudioClip dashSound;
+    
     private AudioSource _audio;
-    private Rigidbody2D _rb2d;
     private Animator _anim;
-    
+
+    private Vector2 _moveInput;
+    private float _timeSinceAttack = 999;
+    private float _timeSinceDash = 999;
+    private bool _isDashing = false;
+
     int attack = Animator.StringToHash("attack");
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
         _audio = GetComponent<AudioSource>();
-        _anim = GetComponent<Animator>();
-
-        _rb2d.velocity = transform.right * forwardSpeed;
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        
+        _anim = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
     {
+        transform.Translate(forwardSpeed * Time.fixedDeltaTime * Vector2.right, Space.World);
+        transform.Translate(verticalSpeed * Time.fixedDeltaTime * _moveInput, Space.World);
+        
+        // rotate a bit up or down or reset, depending on vertical move input
+        // lerping slowly to avoid sudden changes
+        float targetRotation = _moveInput.y * rotationAngle;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, targetRotation), Time.fixedDeltaTime * 20f);
+
+        _timeSinceAttack += Time.fixedDeltaTime;
+        _timeSinceDash += Time.fixedDeltaTime;
+
+        if (_isDashing || _timeSinceDash < minimumDashDuration)
+        {
+            transform.Translate(dashSpeed * Time.fixedDeltaTime * Vector2.right, Space.World);
+        }
     }
 
     protected override void NoInput()
     {
-        /*if (transform.localRotation.eulerAngles != Vector3.zero)
-        {
-            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.identity, 5 * Time.deltaTime);
-        }*/
+        _moveInput = Vector2.zero;
+        _isDashing = false;
     }
 
     protected override void UpInput()
     {
-        transform.Translate(Vector2.up * verticalSpeed * Time.deltaTime);
+        _moveInput = Vector2.up;
     }
 
     protected override void DownInput()
     {
-        transform.Translate(Vector2.down * verticalSpeed * Time.deltaTime);
-    }   
+        _moveInput = Vector2.down;
+    }
 
     protected override void Jump()
     {
         //do dash
-        if (SquashStretch)
+        if (!_isDashing && _timeSinceDash > minimumDashDuration)
         {
-            SquashStretch.PlayFeedbacks();
+            // squash to look more aerodynamic
+            SquashStretch?.PlayFeedbacks();
+            // whoosh sound
+            _audio.PlayOneShot(dashSound);
+            _timeSinceDash = 0;
         }
+        _isDashing = true;
+        // get pushed forward - in FixedUpdate
     }
 
     protected override void Interact()
     {
+        if (_timeSinceAttack < delayBetweenAttacks)
+        {
+            return;
+        }
+        _timeSinceAttack = 0;
         // claws forward
         _anim.SetTrigger(attack);
         //do screech
-        _audio.PlayOneShot(screechSound);
+        _audio.PlayOneShot(screechSounds[UnityEngine.Random.Range(0, screechSounds.Length)]);
     }
 }
